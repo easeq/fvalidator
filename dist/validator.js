84f095645f83bfd23c6d0fc070c60598bb4a7841
ES <eon@earncef.com>
2019-01-27 11:02:48 +0530

    Fixed checkAsync argument.

--- dist/validator.js
+++ dist/validator.js
@@ -1,4 +1,4 @@
-/*! validatorjs - v3.15.0 -  - 2019-01-26 */
+/*! validatorjs - v3.15.0 -  - 2019-01-27 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function AsyncResolvers(onFailedOne, onResolvedAll) {
   this.onResolvedAll = onResolvedAll;
@@ -1523,7 +1523,6 @@ Validator.prototype = {
      * @return {object}
      */
     _parseRules: function(rules) {
-
         var parsedRules = {};
         rules = this._flattenObject(rules);
 
@@ -1534,8 +1533,6 @@ Validator.prototype = {
             this._parseRulesCheck(attribute, rulesArray, parsedRules);
         }
         return parsedRules;
-
-
     },
 
     _parseRulesCheck: function(attribute, rulesArray, parsedRules, wildCardValues) {
@@ -1791,7 +1788,7 @@ Validator.prototype = {
      */
     _validate: function(type, data, callback) {
         this.input = data || {};
-        var async = this._checkAsync('type', callback);
+        var async = this._checkAsync(type, callback);
         if (async) {
             return this.checkAsync(callback);
         }
